c008ef879e84be64e28895f3757ad2ded79417b1
Guillaume Pitois <guillaume.pitois1@gmail.com>
2022-06-27 16:41:00 +0200

    forcer le typage en fonction

--- function/salut.php
+++ function/salut.php
@@ -1,4 +1,4 @@
 <?php
-function toto ($prenom){
+function toto ( /*type de parametre*/$prenom) /* : et le type pour obliger la fonction si on le veut et pour cumuler les types on met | */{
     return "coucou $prenom";
 }
