f1254984641b1a6d06e919b6e8906c2c34dee76e
Guillaume Pitois <guillaume.pitois1@gmail.com>
2022-06-27 10:29:13 +0200

    découverte des diff types de variables et passage de reference

--- index.php
+++ index.php
@@ -7,8 +7,6 @@ echo $toto;
 echo $coucou;
 
 $jour='lundi';
-
-
 // en simple guillemet la valuer de la variable n'est pas affiché
 echo 'nous sommes $jour';
 echo "<br/>";
@@ -20,3 +18,20 @@ echo gettype($jour);
 echo "<br/>";
 // afficher le type + le contenu et la longueur
 var_dump($jour);
+echo "<br/>";
+
+// passage par référence
+$a = 1;
+$b = &$a;
+$b =2;
+echo $a; 
+
+// les différents types de variables
+// string
+// interger
+// float
+// boolean
+// Array
+// Object 
+// Ressource
+// Enumeration ou Enums
