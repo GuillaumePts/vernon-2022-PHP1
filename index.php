251490146e215005378a60a979883c1dfc8e12da
Guillaume Pitois <guillaume.pitois1@gmail.com>
2022-06-27 10:04:13 +0200

    decouvert gettype et var_dump

--- index.php
+++ index.php
@@ -5,3 +5,18 @@ $coucou="<h2>test</h2>";
 $toto="<h1>12</h1>";
 echo $toto;
 echo $coucou;
+
+$jour='lundi';
+
+
+// en simple guillemet la valuer de la variable n'est pas affiché
+echo 'nous sommes $jour';
+echo "<br/>";
+// en double si 
+echo "nous sommes $jour";
+echo "<br/>";
+// afficher le type 
+echo gettype($jour);
+echo "<br/>";
+// afficher le type + le contenu et la longueur
+var_dump($jour);
