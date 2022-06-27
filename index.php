dc295868580bfb1a1f7c03d25a9eaffb488fd7b9
Guillaume Pitois <guillaume.pitois1@gmail.com>
2022-06-27 12:18:08 +0200

    les opérateur de comparaison et les conditions

--- index.php
+++ index.php
@@ -35,3 +35,70 @@ echo $a;
 // Object 
 // Ressource
 // Enumeration ou Enums
+
+
+// les opérateurs
+/*
++
+-
+*
+/
+modulo % = reste d'une division 
+** = (10 puissance 2 : 10 ** 2)
+pour faire racine carré = sqrt(float $num):float
+https://www.php.net/manual/fr/ref.math.php
+
+Opérateur combiné 
+
+$i = $i + 1 ;
+$i++;
+$i+=1
+
+*/ 
+
+
+/*
+Opérateurs de comparaison
+== : compare la valeur
+=== : compare la valeur avec le typage important avec les booléen
+!= ou <> : le contraire  en valeurs
+!== : différent en valeur et en typage
+<=
+>=
+<
+>
+*/
+
+/*
+les conditions
+*/
+
+$x=210;
+
+if($x > 0){
+    echo '$x est positif';
+}elseif($x<0){
+    echo '$x est negatif';
+}else{
+    echo '$x est nul';
+};
+echo "<br/>";
+
+
+$y = "-2";
+
+if(isset($y) && is_numeric($y)){
+    if($x > 0){
+        echo '$x est positif';
+    }elseif($x<0){
+        echo '$x est negatif';
+    }else{
+        echo '$x est nul';
+    };
+    echo "<br/>";
+  }else{
+    echo "La variable y n'est pas définie" . "<br />";
+  }
+
+
+
